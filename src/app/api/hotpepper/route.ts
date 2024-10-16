3fbb707d15d81d5487ad120c848ed950a95689e6
s22012 <s22012@std.it-college.ac.jp>
2024-10-16 11:25:28 +0900

    cssもある程度やった

--- src/app/api/hotpepper/route.ts
+++ src/app/api/hotpepper/route.ts
@@ -4,11 +4,19 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
-    const keyword = searchParams.get('q');
+    const keyword = searchParams.get('q') || ''; // 検索キーワードが無い場合、空文字列に設定
     const apikey = process.env.SEARCH_API_KEY; // あなたのホットペッパーAPIキー
 
-    const url = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${apikey}&keyword=${encodeURIComponent(keyword || '')}&format=json`;
+    // パラメータの組み立て
+    const params = new URLSearchParams({
+        key: apikey || '', // APIキーがない場合、空文字列に設定
+        keyword: keyword,
+        format: 'json'
+    });
 
+    const url = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?${params}`;
+
+    // APIリクエストを送信
     const res = await fetch(url);
     const data = await res.json();
 
