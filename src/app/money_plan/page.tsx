9ce007f25f3c5c5e6fa7087598472b169619abdd
s22012 <s22012@std.it-college.ac.jp>
2024-10-29 16:21:09 +0900

    directory作成

---
+++ src/app/money_plan/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+export default function money_plan() {
+ 
+
+
+
+
+    return(
+        <div>
+        <h1> 食費管理 </h1>
+
+        </div>
+    )
+}
