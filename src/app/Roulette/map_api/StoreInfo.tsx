63e4c9062edce4f5cb3932bb3cae9fc76a5e2817
its22013 <s22013@std.it-college.ac.jp>
2024-11-11 19:12:43 +0900

    お気に入りの追加

--- src/app/Roulette/map_api/StoreInfo.tsx
+++ src/app/Roulette/map_api/StoreInfo.tsx
@@ -4,11 +4,17 @@ import { RiMoneyCnyCircleFill } from 'react-icons/ri';
 import { IoTimeOutline } from 'react-icons/io5';
 import { IoMdHeart } from 'react-icons/io';
 
+interface StoreInfoProps {
+  store: Store;
+  addToFavorites: () => void;
+  isFavorite: boolean;
+}
 
 interface Store {
   name: string;
   address: string;
   phone: string;
+  genre: string;
   budget?: {
     code: string;
     name: string;
@@ -23,10 +29,8 @@ interface Store {
   open: string;
 }
 
-const StoreInfo: React.FC<{ store: Store }> = ({ store }) => {
-  // Google Mapsリンク用のエンコードされた住所URLを生成
+const StoreInfo: React.FC<StoreInfoProps> = ({ store, addToFavorites, isFavorite }) => {
   const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(store.address)}`;
-  // 営業時間から括弧以降のテキストを除外
   const displayOpenHours = store.open.split('（')[0].trim();
 
   return (
@@ -39,31 +43,32 @@ const StoreInfo: React.FC<{ store: Store }> = ({ store }) => {
             className={styles.store_photo} 
           />
         )}
-        {/* 営業時間 */}
         <div className={styles.Business_hours}>
-        <IoTimeOutline />
-        <h2>営業時間</h2> 
+          <IoTimeOutline />
+          <h2>営業時間</h2>
         </div>
         <div className={styles.Business_text}>
-        {displayOpenHours} 
+          {displayOpenHours} 
         </div>
       </div>
     
       <div className={styles.text_container}>
         <h1 className={styles.Restauran_title}>{store.name}</h1><br />
-
+        <h3 className={styles.genre_text}>#{store.genre}</h3> 
         <p className={styles.average_text}>
           <RiMoneyCnyCircleFill />{store.budget?.name}
         </p><br />
-
+        
         <div className={styles.sub_container}>
-        {/* Google Maps リンクを「マップで表示」に変更 */}
-        <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer" className={styles.address_link}>
-        　マップで表示
-        </a>
-        <div className={styles.heart_icon}>
-        <IoMdHeart />
-        </div>
+          <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer" className={styles.address_link}>
+            マップで表示
+          </a>
+          <div 
+            className={`${styles.heart_icon} ${isFavorite ? styles.favorited : ''}`} 
+            onClick={addToFavorites}
+          >
+            <IoMdHeart />
+          </div>
         </div>
       </div>
     </div>
